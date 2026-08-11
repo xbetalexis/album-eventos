4505a44326ec02cf6e874a2f5c73e9ed52388895
XBET5 <221407242+xbetalexis@users.noreply.github.com>
2026-08-11 14:09:08 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,14 +1,14 @@
 const EVENTOS = {
-  "sophia-xv": {
+  "valentina-xv": {
     tipo: "MIS XV",
-    nombre: "Sophia",
+    nombre: "Valentina",
     fecha: "6 de noviembre de 2026",
 
     mensaje:
       "Esta noche está llena de momentos únicos. Ayudame a guardar cada recuerdo compartiendo las fotos que captures durante mis XV ✨",
 
-album: "https://photos.app.goo.gl/jWcrbgYo6iV4iAWt8",
+    album: "https://photos.app.goo.gl/jWcrbgYo6iV4iAWt8",
 
-fondo: "/fondos/valenxv.png"
+    fondo: "/fondos/valenxv.png"
   }
 };
